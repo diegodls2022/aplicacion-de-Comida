e392658cfa05e3555f19a6d366ba1b5657e64390
Diego <Diegodls_20@hotmail.com>
2023-04-05 13:52:52 -0300

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import  { QuioscoProvider }  from '../../context/QuioscoProvider'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <QuioscoProvider>
+        <Component {...pageProps} />
+    </QuioscoProvider>
+  )
 }
+
+export default MyApp;
+
